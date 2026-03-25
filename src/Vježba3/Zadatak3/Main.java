247c14a4169eb019d36eb2cace13a5321e0a328a
Mario Barisa <mario@barisa.me>
2026-03-25 13:43:12 +0100

    .

--- src/Vježba3/Zadatak3/Main.java
+++ src/Vježba3/Zadatak3/Main.java
@@ -10,5 +10,6 @@ public class Main {
 
            System.out.println(Ivan.izracujanPlacu());
            System.out.println(Popsi.izracujanPlacu());
+           //System.out.println(Popsi.izracujanPlacu());
            }
 }
